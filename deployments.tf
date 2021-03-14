2394197d86972d4089a5d5a8924a050ef55ad66c
Dylan Turnbull <dylanturn@gmail.com>
2021-03-14 10:10:33 -0500

    fixed env var name

--- deployments.tf
+++ deployments.tf
@@ -8,27 +8,27 @@ module "traefik_oauth_endpoint" {
   labels    = var.labels
 
   containers = [{
-      name = "oauth"
-      image_repository  = "registry.hub.docker.com"
-      image_name        = "funkypenguin/traefik-forward-auth"
-      image_tag         = "latest"
-      image_pull_policy = "Always"
-      is_init           = false
-      environment_variables = {
-        "AUTH_HOST"     = var.oauth_host
-        "COOKIE_DOMAIN" = var.oauth_cookie_domain
-        "CLIENT_ID"     = var.oauth_client_id
-        "OIDC_ISSUER"   = var.oauth_oidc_issuer
+    name              = "oauth"
+    image_repository  = "registry.hub.docker.com"
+    image_name        = "funkypenguin/traefik-forward-auth"
+    image_tag         = "latest"
+    image_pull_policy = "Always"
+    is_init           = false
+    simple_environment_variables = {
+      "AUTH_HOST"     = var.oauth_host
+      "COOKIE_DOMAIN" = var.oauth_cookie_domain
+      "CLIENT_ID"     = var.oauth_client_id
+      "OIDC_ISSUER"   = var.oauth_oidc_issuer
+    }
+    secret_environment_variables = {
+      "CLIENT_SECRET" = {
+        name = kubernetes_secret.middleware_oauth.metadata[0].name
+        key  = "client_secret"
       }
-      secret_environment_variables = {
-        "CLIENT_SECRET" = {
-          name = kubernetes_secret.middleware_oauth.metadata[0].name
-          key  = "client_secret"
-        }
-        "SECRET" = {
-          name = kubernetes_secret.middleware_oauth.metadata[0].name
-          key  = "cookie_secret"
-        }
+      "SECRET" = {
+        name = kubernetes_secret.middleware_oauth.metadata[0].name
+        key  = "cookie_secret"
       }
-    }]
+    }
+  }]
 }
